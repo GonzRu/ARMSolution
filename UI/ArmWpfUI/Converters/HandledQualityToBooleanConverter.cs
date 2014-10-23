0585c4c72f50c197a7def9f7d5535eb19a01a6d8
Smirnov Denis <gonzru@gmail.com>
2014-10-23 09:47:21 +0400

    refact HandledQualityToBooleanConverter

--- UI/ArmWpfUI/Converters/HandledQualityToBooleanConverter.cs
+++ UI/ArmWpfUI/Converters/HandledQualityToBooleanConverter.cs
@@ -1,33 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
 using System.Windows.Data;
 using CoreLib.Models.Configuration;
+using System;
+using System.Globalization;
+using UICore.Converters;
 
 namespace ArmWpfUI.Converters
 {
-    class HandledQualityToBooleanConverter : IValueConverter
+    [ValueConversion(typeof(Tuple<object, TagValueQuality>), typeof(bool))]
+    class HandledQualityToBooleanConverter : ConverterBase
     {
-        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (!(value is Tuple<object, TagValueQuality>))
-                return false;
+        public bool Invert { get; set; }
 
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
             var tagValueQuality = (value as Tuple<object, TagValueQuality>).Item2;
 
-            bool invert = false;
-            if (parameter is bool)
-                if ((bool) parameter)
-                    invert = true;
-
-            return tagValueQuality == TagValueQuality.vqHandled || tagValueQuality == TagValueQuality.vqCalculatedHanle ? !invert : invert;
-        }
-
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
+            return (tagValueQuality == TagValueQuality.vqHandled || tagValueQuality == TagValueQuality.vqCalculatedHanle) ^ Invert;
         }
     }
 }
