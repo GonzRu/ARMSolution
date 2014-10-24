63535c2d1f0cb3734a9c894a7c073b3ce436bf7a
Smirnov Denis <gonzru@gmail.com>
2014-10-24 16:28:22 +0400

    add window to show uploading progress

---
+++ UI/ArmWpfUI/Views/TerminalViews/UpLoadDocumentView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ArmWpfUI.ViewModels;
+using UICore.Commands;
+
+namespace ArmWpfUI.Views.TerminalViews
+{
+    /// <summary>
+    /// Логика взаимодействия для UpLoadDocumentView.xaml
+    /// </summary>
+    public partial class UpLoadDocumentView
+    {
+        private DeviceViewModel _deviceViewModel;
+
+        public UpLoadDocumentView(DeviceViewModel deviceViewModel)
+        {
+            InitializeComponent();
+
+            _deviceViewModel = deviceViewModel;
+            DataContext = _deviceViewModel;
+            _deviceViewModel.UploadDocumentAsyncCommand.Executed += OnExecuted;
+            _deviceViewModel.UploadDocumentAsyncCommand.Cancelled += OnExecuted;
+        }
+
+        private void OnExecuted(object sender, CommandEventArgs args)
+        {
+            _deviceViewModel.UploadDocumentAsyncCommand.Executed -= OnExecuted;
+            _deviceViewModel.UploadDocumentAsyncCommand.Cancelled -= OnExecuted;
+            Close();
+        }
+    }
+}
