ed6c0df23f4a2fad2f7e0227a5b3a2dc5db60f1f
Smirnov Denis <gonzru@gmail.com>
2014-10-24 09:46:48 +0400

    add document model

---
+++ Core/CoreLib/Models/Common/Document.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoreLib.Models.Common
+{
+    public class Document
+    {
+        /// <summary>
+        /// Идентификатор документа в системе
+        /// </summary>
+        public int DocumentId { get; set; }
+
+        /// <summary>
+        /// Дата добавления документа
+        /// </summary>
+        public DateTime DocumentAddDate { get; set; }
+
+        /// <summary>
+        /// Имя пользователя, добавившего документ
+        /// </summary>
+        public string DocumentUserName { get; set; }
+
+        /// <summary>
+        /// Имя документа
+        /// </summary>
+        public string DocumentFileName { get; set; }
+
+        /// <summary>
+        /// Комментарий к документу
+        /// </summary>
+        public string DocumentComment { get; set; }
+    }
+}
