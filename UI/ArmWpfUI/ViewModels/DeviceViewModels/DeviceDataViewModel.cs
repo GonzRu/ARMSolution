b5571fcdcc457faf29caaea5c1f61fff9fb53bb2
Smirnov Denis <gonzru@gmail.com>
2014-11-07 10:21:43 +0300

    add DeviceDataViewModel

---
+++ UI/ArmWpfUI/ViewModels/DeviceViewModels/DeviceDataViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CoreLib.ExchangeProviders;
+using CoreLib.Models.Configuration;
+using System.Collections.Generic;
+using UICore.ViewModels;
+
+namespace ArmWpfUI.ViewModels.DeviceViewModels
+{
+    public class DeviceDataViewModel : ViewModelBase
+    {
+
+        #region Public properties
+
+        /// <summary>
+        /// Список групп. На данный момент не предполагает динамического измененияs
+        /// </summary>
+        public List<Group> Groups { get; set; } 
+
+        #endregion
+
+        #region Private fields
+
+        private IExchangeProvider _exchangeProvider;
+
+        #endregion
+
+        #region Constructors
+
+        public DeviceDataViewModel(List<Group> groups, IExchangeProvider exchangeProvider)
+        {
+            Groups = groups;
+            _exchangeProvider = exchangeProvider;
+        }
+
+        #endregion
+    }
+}
