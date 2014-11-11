c92dc4ffd91fd79dad93794a0e2e6fb5b3259877
Smirnov Denis <gonzru@gmail.com>
2014-11-11 10:22:09 +0300

    add empty DeviceDocumentsViewModel

---
+++ UI/ArmWpfUI/ViewModels/DeviceViewModels/DeviceDocumentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using CoreLib.Models.Common;
+using UICore.ViewModels;
+
+namespace ArmWpfUI.ViewModels.DeviceViewModels
+{
+    public class DeviceDocumentsViewModel : ViewModelBase
+    {
+
+    }
+}
