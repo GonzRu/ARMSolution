15b93dac06c328c3b22ede09bdafc4bce95de701
Smirnov Denis <gonzru@gmail.com>
2014-11-11 16:57:25 +0300

    добавил модель SettingSet, описывающую архивный набор уставок устройства

---
+++ Core/CoreLib/Models/Common/SettingsSet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CoreLib.Models.Common
+{
+    /// <summary>
+    /// Описывает архивный набор уставок
+    /// </summary>
+    public class SettingsSet
+    {
+        /// <summary>
+        /// Идентификатор набора уставок
+        /// </summary>
+        public Int32 SettingsSetId { get; set; }
+
+        /// <summary>
+        /// Комментарий к набору уставок
+        /// </summary>
+        public String SettingsSetComment { get; set; }
+
+        /// <summary>
+        /// Дата записи набора уставок
+        /// </summary>
+        public DateTime SettingsSetDateTime { get; set; }
+    }
+}
